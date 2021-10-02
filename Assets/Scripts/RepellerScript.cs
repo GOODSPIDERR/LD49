a69393219fe61959dc3edb6fbca2379ad06c1c45
Sarah Assmus <84595949+sasstrologer@users.noreply.github.com>
2021-10-02 19:18:09 -0400

    Main Menu repeller script finished

--- Assets/Scripts/RepellerScript.cs
+++ Assets/Scripts/RepellerScript.cs
@@ -2,54 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class RepellerScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Vector3 mousePosition;
-    private Rigidbody2D button;
-    private Vector2 direction;
-    private float moveSpeed = 200f;
-    private bool mouse_over = false;
-    private Vector3 buttonStartPos;
 
-    void Start()
-    {
-        button = GetComponent<Rigidbody2D>();
-        buttonStartPos = transform.position;
-
-    }
-
+    [SerializeField] Vector2 startPosition; 
+    [SerializeField] Vector2 endPosition; 
+    float duration = 2f;
 
     public void Repel()
     {
-
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction = (transform.position - mousePosition).normalized;
-            Vector2 newPos = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
-            button.velocity = newPos;
+        transform.DOLocalMove(endPosition, duration, false);
     }
 
     public void Comeback()
     {
-
-        direction = (buttonStartPos - transform.position).normalized;
-        Vector2 newPos = new Vector2(direction.x, direction.y);
-        button.velocity = newPos;
-
-
+        transform.DOLocalMove(startPosition, duration, false);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        mouse_over = true;
-        Debug.Log("Mouse enter");
         Repel();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        mouse_over = false;
-        Debug.Log("Mouse exit");
         Comeback();
     }
 }
